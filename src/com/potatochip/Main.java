9980cf0295b22d20809680ff41ef4682225914bd
Erwin-R <rerwin684@gmail.com>
2023-12-07 09:24:40 -0600

    added git files for project and edited main class

---
+++ src/com/potatochip/Main.java
@@ -0,0 +1,23 @@
+package com.potatochip;
+
+public class Main {
+    public static void main(String[] args) {
+        int numberOne = 10;
+        int numberTwo = 3;
+        System.out.println(numberOne + numberTwo);
+        System.out.println(numberOne - numberTwo);
+        System.out.println(numberOne * numberTwo);
+        System.out.println(numberOne / numberTwo);
+        System.out.println(numberOne % numberTwo);
+        System.out.println((2+2) * (3/1) * 2);
+        /*
+        BODMAS
+        B - Brackets
+        O - Orders(powers/indices or roots)
+        D - Division
+        M - Multiplication
+        A - Addition
+        S - Subtraction
+         */
+    }
+}
